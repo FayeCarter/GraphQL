88704f07bcd9c4ec93a482bd460d3c61085160e0
FayeCarter <fayecarter333@gmail.com>
2020-07-03 17:37:49 +0100

    Render Apollo query

--- Books/client/src/components/BookList.js
+++ Books/client/src/components/BookList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {gql} from "apollo-boost";
 import { graphql } from "react-apollo";
 
@@ -11,14 +11,30 @@ const getBooksQuery = gql`
   }
 `
 
-function BookList() {
-  return (
-    <div>
-      <ul id="book-list">
-        <li>Book Name</li>
-      </ul>
-    </div>
-  );
+class BookList extends Component {
+
+  displayBooks(){
+    var data = this.props.data;
+    if( data.loading ) {
+      return( <div>Loading Books</div> )
+    } else {
+      return data.books.map( book => {
+        return(
+          <li>{ book.name }</li>
+        )
+      })
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          { this.displayBooks() }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default graphql( getBooksQuery )( BookList );
