cbe56b6b8ae4991fd054df5fd04d2385dedbe41b
FayeCarter <fayecarter333@gmail.com>
2020-07-03 13:10:51 +0100

    Add mongoose to dependencies

--- Books/server/app.js
+++ Books/server/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const schema = require("./schema/schema")
+const schema = require("./schema/schema");
+constmongoose = require("mongoose");
 
 const app = express();
+
+
 
 app.use("/graphql", graphqlHTTP({
   schema,
