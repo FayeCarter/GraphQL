c734a6c1efd9a6381849d3e34fb1438c00c127cd
FayeCarter <fayecarter333@gmail.com>
2020-07-03 17:28:07 +0100

    Add cors to server

--- Books/server/app.js
+++ Books/server/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors")
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors);
 
 mongoose.connect(process.env.CONNECTION_STRING, { 
   useNewUrlParser: true, 
