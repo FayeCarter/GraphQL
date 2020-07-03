b2a8012d47e18e0936e788cd621505899d378845
FayeCarter <fayecarter333@gmail.com>
2020-07-03 16:52:41 +0100

    Create BookList component

--- Books/client/src/App.js
+++ Books/client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import BookList from "./components/BookList"
+
 function App() {
   return (
     <div className="App">
-     <h1>Book Keep</h1>
+      <h1>Book Keep</h1>
+      <BookList />
     </div>
   );
 }
