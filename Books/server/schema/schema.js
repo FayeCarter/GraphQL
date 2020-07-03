f8837813c66fe32fc7f644de14300b8f98005fd1
FayeCarter <fayecarter333@gmail.com>
2020-07-03 12:30:26 +0100

    Add Author relation to booy

--- Books/server/schema/schema.js
+++ Books/server/schema/schema.js
@@ -26,7 +26,13 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: { 
+      type: AuthorType,
+      resolve(parent, args){
+        return _.find(authors, { id: parent.authorID });
+      }
+    }
   })
 });
 
