dc88953027d3662fbe4d9a319fdf8998ca511ec0
FayeCarter <fayecarter333@gmail.com>
2020-07-06 10:45:28 +0100

    Move function out of render

--- Books/client/src/components/BookDetails.js
+++ Books/client/src/components/BookDetails.js
@@ -3,37 +3,37 @@ import { graphql } from "react-apollo";
 import  { getBookQuery } from "../queries/queries"
 
 class BookDetails extends Component {
+  displayBookDetails() {
+    const { book } = this.props.data;
+    if ( book ) {
+      return(
+        <div>
+          <h2>Title: { book.name }</h2>
+          <p>Genre: { book.genre }</p>
+          <p>Author: { book.author.name }</p>
+          <p>More from { book.author.name }: </p>
+          <ul className="other-books">
+            {
+              book.author.books.map( item => {
+                return (
+                  <li key={ item.id }>{ item.name }</li>
+                )
+              })
+            }
+          </ul>
+        </div>
+      )
+    } else {
+      return (
+        <div>Select a book for details</div>
+      )
+    }
+  };
+  
   render() {
-    const displayBookDetails = () => {
-      const { book } = this.props.data;
-      if ( book ) {
-        return(
-          <div>
-            <h2>Title: { book.name }</h2>
-            <p>Genre: { book.genre }</p>
-            <p>Author: { book.author.name }</p>
-            <p>More from { book.author.name }: </p>
-            <ul className="other-books">
-              {
-                book.author.books.map( item => {
-                  return (
-                    <li key={ item.id }>{ item.name }</li>
-                  )
-                })
-              }
-            </ul>
-          </div>
-        )
-      } else {
-        return (
-          <div>Select a book for details</div>
-        )
-      }
-    };
-
     return (
       <div id="book-details">
-        { displayBookDetails() }
+        { this.displayBookDetails() }
       </div>
     );
   }
