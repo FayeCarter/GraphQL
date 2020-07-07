74e5189300adbd81ed55468dbe9f4dcbbde741b4
FayeCarter <fayecarter333@gmail.com>
2020-07-07 13:32:03 +0100

    Add fixtures file for tests

---
+++ Hacker_News/client/src/fixtures/index.js
@@ -0,0 +1,17 @@
+export const singularStory = {
+  by: "Faye Carter",
+  id: 1,
+  time: 1567209822,
+  title: "Test Story",
+  url: "https://something.com/news",
+};
+
+export const storyIds = [1];
+
+export const emptySingularStory = {
+  by: undefined,
+  id: undefined,
+  time: undefined,
+  title: undefined,
+  url: undefined,
+}
