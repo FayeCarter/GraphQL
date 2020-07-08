5a6604841a1dbfefbbd9eb946dec68fcd2660938
FayeCarter <fayecarter333@gmail.com>
2020-07-08 14:27:03 +0100

    Add get Articles by id query

--- Hacker_News/server/resolvers/index.js
+++ Hacker_News/server/resolvers/index.js
@@ -2,5 +2,8 @@ export const resolvers = {
   Query: {
     articleBySource: (_, { id, source }, { dataSources }) =>
       dataSources[source].getArticle(id, source),
+
+    articlesBySource: (_, { ids, source }, { dataSources }) =>
+      dataSources[source].getArticleByIds(ids),
   }
 }
