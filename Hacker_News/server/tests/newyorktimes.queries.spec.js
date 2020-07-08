fa1cf191aedbd1590ad95ede5a18560b12aff56b
FayeCarter <fayecarter333@gmail.com>
2020-07-08 17:13:25 +0100

    Test NYT get all articles

--- Hacker_News/server/tests/newyorktimes.queries.spec.js
+++ Hacker_News/server/tests/newyorktimes.queries.spec.js
@@ -1,5 +1,52 @@
-describe("[Empty", () => {
-  it("empty", () => {
-    expect(1+1).toEqual(2)
+import { createTestClient } from 'apollo-server-testing';
+import { ApolloServer, gql } from 'apollo-server';
+import { NewYorkTimesAPI } from "../datasources/newyorktimes";
+import { resolvers } from '../resolvers';
+import { typeDefs } from '../typedefs';
+import { getArticlePreReducerStub } from '../fixtures/newyorltimes';
+
+const GET_ALL_ARTICLES_BY_SOURCE = gql`
+  query getArticlesBySource($source: String!) {
+    allArticlesBySource(source: $source) {
+      id
+      title
+      author
+      url
+      time
+      source
+    }
+  }
+`;
+
+const constructTestServer = () => {
+  const newYorkTimesAPI = new NewYorkTimesAPI();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+      newyorktimes: newYorkTimesAPI
+    })
+  });
+
+  return { server, newYorkTimesAPI };
+}
+
+describe('[Queries.NewYorkTimesAPI]', () => {
+  it('fetches an array of articles from the NewYorkTimes API', async () => {
+    const { server, newYorkTimesAPI } = constructTestServer();
+
+    newYorkTimesAPI.get = jest.fn(() => ({
+      results: [getArticlePreReducerStub],
+    }));
+
+    const { query } = createTestClient(server);
+
+    const response = await query({
+      query: GET_ALL_ARTICLES_BY_SOURCE,
+      variables: { ids: [21168364], source: 'newyorktimes' },
+    });
+
+    expect(response).toMatchSnapshot();
   });
 });
